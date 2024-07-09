a4c90e8b94f9ea73b0e511dc932d82d9a582bd2b
DaniCalderon <mesiasd.calderon@gmail.com>
2024-07-09 18:02:34 -0400

    Nueva api CountryApp, avanzando hasta la clase 119

---
+++ 04-countryApp/src/app/countries/countries.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCapitalPagesComponent } from './pages/by-capital-pages/by-capital-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    ByCapitalPagesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
