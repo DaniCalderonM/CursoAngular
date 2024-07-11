319cfcd529a23bfd94798dbef5f4b716f02644f1
DaniCalderon <mesiasd.calderon@gmail.com>
2024-07-11 10:07:43 -0400

    fin de la seccion 10 y de la api countries

--- 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
@@ -7,12 +7,19 @@ import { CountriesService } from '../../services/countries.service';
   templateUrl: './by-country-page.component.html',
   styles: ``
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit {
 
   public countries: Country[] = [];
+  // aqui deberia ir lo del loading
+  public initialValue:string = '';
 
   //Inyectar el servicio
   constructor (private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountry.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountry.term;
+  }
 
   searchByCountry( term:string ):void {
     this.countriesService.searchCountry(term).subscribe(
--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, delay, map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+
 import { Country } from '../interfaces/country.interface';
 import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
@@ -16,9 +18,19 @@ export class CountriesService {
   }
 
 
+  constructor(private httpClient: HttpClient) {
+    this.loadFromLocalStorage();
+  }
 
+  private saveToLocalStorage() {
+    localStorage.setItem( 'cacheStore', JSON.stringify(this.cacheStore) );
+  }
 
-  constructor(private httpClient: HttpClient) { }
+  private loadFromLocalStorage() {
+    if ( !localStorage.getItem('cacheStore') ) return;
+
+    this.cacheStore = JSON.parse( localStorage.getItem('cacheStore')! );
+  }
 
   //Refactorizando codigo
   private getCountriesRequest( url:string ): Observable<Country[]>{
@@ -42,19 +54,31 @@ export class CountriesService {
   searchCapital( term: string ): Observable<Country[]>{
 
     const url = `${ this.apiUrl }/capital/${ term }`;
-    return this.getCountriesRequest( url );
+    return this.getCountriesRequest( url )
+    .pipe(
+      tap( countries => this.cacheStore.byCapital = { term, countries }),
+      tap( () => this.saveToLocalStorage())
+    );
   }
 
   searchCountry( term: string ): Observable<Country[]>{
 
     const url = `${ this.apiUrl }/name/${ term }`;
-    return this.getCountriesRequest( url );
+    return this.getCountriesRequest( url )
+    .pipe(
+      tap( countries => this.cacheStore.byCountry = { term, countries }),
+      tap( () => this.saveToLocalStorage())
+    );
   }
 
-  searchRegion( region: string ): Observable<Country[]>{
+  searchRegion( region: Region ): Observable<Country[]>{
 
     const url = `${ this.apiUrl }/region/${ region }`;
-    return this.getCountriesRequest( url );
+    return this.getCountriesRequest( url )
+    .pipe(
+      tap( countries => this.cacheStore.byRegion = { region, countries }),
+      tap( () => this.saveToLocalStorage())
+    );
   }
 
 
