066b1882939851ba7cd094165785d11152bc9d38
Danicsa Calderon <mesiasd.calderon@gmail.com>
2024-07-15 18:16:38 -0400

    subiendo seccion 16,17 y 18

---
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from '../../../environments/environments';
+import { User } from '../interfaces/user.interface';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser(): User|undefined {
+    if ( !this.user) return undefined;
+    //Devolvemos un deep clone
+    return structuredClone( this.user );
+  }
+
+
+  login( email: string, password: string ): Observable<User> {
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+    .pipe(
+      tap( user => this.user = user ) ,
+      tap( user => localStorage.setItem('token', 'Askjsd3d548fsd' )),
+    )
+  }
+
+
+  checkAuthentication(): Observable<boolean> | boolean{
+
+    if ( !localStorage.getItem('token') ) return false;
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+    .pipe(
+      tap( user => this.user = user),
+      map( user => !!user),
+      catchError( err => of(false) )
+    );
+
+
+  }
+
+
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+}
